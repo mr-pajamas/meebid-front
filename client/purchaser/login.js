69a441c28697186cc48e3cbf98c9f3cb21b473d5
laolong1987 <232886019@qq.com>
2016-11-27 10:40:04 +0800

    no message

---
+++ client/purchaser/login.js
@@ -0,0 +1,33 @@
+Template.p_login.events({
+
+  "submit": function (event, template) {
+    event.preventDefault();
+    if (Meteor.user()) Meteor.logout();
+
+    var username = template.$("[name=username]").val();
+    var password = template.$("[name=password]").val();
+
+    if (!Match.test(username, Pattern.NonEmptyString)) {
+      alert("请正确填写账号");
+      return;
+    }
+
+    if (!Match.test(password, Pattern.NonEmptyString)) {
+      alert("请输入密码");
+      return;
+    }
+
+    var options = {
+      username: username,
+      password: password
+    };
+
+    Meteor.commonLogin(options, function (error) {
+      if (error) {
+        alert(error.reason);
+      } else {
+        FlowRouter.go("index");
+      }
+    });
+  }
+});
---
+++ client/seller/login.js
@@ -0,0 +1,36 @@
+Template.s_login.events({
+
+  "submit": function (event, template) {
+    event.preventDefault();
+    if (Meteor.user()) Meteor.logout();
+
+    var username = template.$("[name=username]").val();
+    var password = template.$("[name=password]").val();
+
+    if (!Match.test(username, Pattern.NonEmptyString)) {
+      alert("请正确填写账号");
+      return;
+    }
+
+    if (!Match.test(password, Pattern.NonEmptyString)) {
+      alert("请输入密码");
+      return;
+    }
+
+    var options = {
+      username: username,
+      password: password,
+      type: 's'
+    };
+
+
+
+    Meteor.commonLogin(options, function (error) {
+      if (error) {
+        alert(error.reason);
+      } else {
+        FlowRouter.go("index");
+      }
+    });
+  }
+});
